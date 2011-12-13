90a323d8be01fdb84ecd1b450d225948d8fc2fa4
Matthew Kenny <matthewkenny@gmail.com>
2011-12-12 20:31:16 -0800

    dynamic addition of leaflet styling with jquery.

--- sampling/static/js/mapcode.js
+++ sampling/static/js/mapcode.js
@@ -1,5 +1,4 @@
 //Load JSON URL and fire callback creating map
-
 var geoJSONBaseURL = "http://localhost:8000/company/geojson/boundary/";
 var geoJSONURL = geoJSONBaseURL + boundary_id;
 
@@ -7,19 +6,52 @@ $.getJSON(geoJSONURL, makeMap);
 
 function makeMap(data){
   //Assign GeoJSON to variable
-  var sitePoly = data
+  var sitePoly = data;
+  
+  //Generic Boundary Styling
+  var boundaryStyle = {
+                    "properties":{
+                       "style":{
+                         "color": "#FF0000",
+                         "opacity": 0.6,
+                         "fillOpacity": 0,
+                         "clickable": false
+                      }
+                    }
+                  };
+
+  //Extend GeoJSON with styling information
+  //console.debug(sitePoly);
+  $.each(sitePoly.features, function(key, value){
+     //console.debug("Key: " + key + ", Value: " + value);
+     $.extend(true, value, boundaryStyle);
+   }
+  )
+
 
   //Create map instance
   var map = new L.Map('map')
-  var boundaryGeoJSON = new L.GeoJSON(sitePoly);
+  var boundaryGeoJSON = new L.GeoJSON();
 
+  //Event Listener for Addition of Data to Boundary Layer
+  boundaryGeoJSON.on("featureparse", function (e){
+    if (e.properties && e.properties.style && e.layer.setStyle){
+        // The setStyle method isn't available for Points. More on that below ...
+        e.layer.setStyle(e.properties.style);
+    }
+  });
+
+  //Populate Boundary Layer with Data
+  boundaryGeoJSON.addGeoJSON(sitePoly);
+
+  //Road Layer
   var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/5df4a1ed4a8c41bd91725ad594aa6139/997/256/{z}/{x}/{y}.png'
-  var cloudmadeAttrib = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade'
+  var cloudmadeAttrib = 'Map data &copy; 2011 OSM contributors, Style &copy; 2011 CloudMade'
   var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
 
-  // See: http://developer.mapquest.com/web/products/open/map
+  //See: http://developer.mapquest.com/web/products/open/map
   var mapQuestAerialURL = 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png'
-  var mapQuestAerialAttrib = 'Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency. Thanks MapQuest!'
+  var mapQuestAerialAttrib = 'Portions Courtesy NASA/JPL-CalTech, USDA FSA. Thanks MapQuest!'
   var mapQuestAerial = new L.TileLayer(mapQuestAerialURL, {
                                                            maxZoom: 18, 
                                                            attribution: mapQuestAerialAttrib, 
