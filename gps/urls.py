0bf772818abfa4c9275f62e0acb4d414e00f5cc6
Matthew Kenny <matthewkenny@gmail.com>
2012-01-02 15:53:19 -0800

    geojson view for gps_points

--- gps/urls.py
+++ gps/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from gps.models import *
+from gps.models import GPS_Point, GPS_Metadata
 
 urlpatterns = patterns('gps.views',
     # Index of the sampling application
     url(r'^index/$', 'index'),
-
+    url(r'^geojson/points/(?P<gps_metadata_id>\d+)/$', 'gps_points'),
 )
--- gps/views.py
+++ gps/views.py
@@ -3,8 +3,20 @@ from vectorformats.Formats import Django, GeoJSON
 from django.http import HttpResponse
 from django.template import Context, RequestContext, loader
 from django.shortcuts import render_to_response
+from gps.models import GPS_Metadata, GPS_Point
 
 # Uses render_to_response()
 def index(request):
     return render_to_response('gps/index.html', {}, context_instance=RequestContext(request))
 
+"""
+Return GPS Points for a given event.
+TODO: Maybe turn into a factory function accepting
+      different geometry types.
+"""
+def gps_points(request, gps_metadata_id):
+    gps_data = GPS_Point.objects.filter(gps_metadata__pk=gps_metadata_id)
+    djf = Django.Django(geodjango="geom", properties=['name','feat_name'])
+    geoj = GeoJSON.GeoJSON()
+    s = geoj.encode(djf.decode(gps_data))
+    return HttpResponse(s)
